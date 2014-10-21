8784daf4df56144009b168457726af00fa5e2091
Givi Kuchukhidze <givi.kuchukhidze@gmail.com>
2014-10-21 10:02:58 +0400

    add wings effect, complete about page

--- js/load.js
+++ js/load.js
@@ -11,5 +11,61 @@ function onReady(callback) {
 onReady(function () {
 
 	$('body').css('display', 'block');
+	$('#wings').css({'opacity' : '0'});
+	$('#social').css({'opacity' :'0'});
+
+	var ind = 0;
+
+
+	// registering custom effect
+	$.cssEase['bounce'] = 'cubic-bezier(0,1,0.5,1.3)';
+
+	var int = setInterval(function(){
+		if(!ind){
+			$('#renard .logo').transition({
+				perspective: '700px', rotateY: '20deg'
+			}, 900);
+			ind = 1;
+		}
+		else{
+			$('#renard .logo').transition({
+				perspective: '700px',  rotateY: '-20deg'
+			}, 900);
+			ind = 0;
+		}
+
+	}, 1000);
+
+
+	setTimeout(function(){
+		clearInterval(int);
+
+		$('#renard .logo').transition({
+			perspective: '1000px', rotateY: '0deg', duration: 500
+		});
+
+		$('#wings, #social').transition({ opacity: 1, duration: 1500 });
+
+		// TOP
+		$('.wing-top').transition({ y: '-=5', duration: 0 });
+		$('.wing-top').transition({ y: '+=5', duration: 2000 });
+
+		// LEFT TOP
+		$('.wing-left-top').transition({ x: '-=10', duration: 0 });
+		$('.wing-left-top').transition({ x: '+=10', duration: 1000 });
+
+		// RIGHT TOP
+		$('.wing-right-top').transition({ x: '+=10', duration: 0 });
+		$('.wing-right-top').transition({ x: '-=10', duration: 1000 });
+
+		// LEFT BOTTOM
+		$('.wing-left-bottom').transition({ x: '-=10', rotate: '-=2deg', duration: 0 });
+		$('.wing-left-bottom').transition({ x: '+=10', rotate: '+=2deg', duration: 2000 });
+
+		// RIGHT BOTTOM
+		$('.wing-right-bottom').transition({ x: '+=10', rotate: '+=2deg', duration: 0 });
+		$('.wing-right-bottom').transition({ x: '-=10', rotate: '-=2deg', duration: 2000 });
+
+	}, 4000);
 
 });
